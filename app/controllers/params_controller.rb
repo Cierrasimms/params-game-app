be127f31c46d3a0651f33820c27f4079647c57eb
CierraSimms <cierrasimms98@gmail.com>
2022-03-09 19:50:54 -0500

    prints off name in all caps

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,8 +1,7 @@
 class ParamsController < ApplicationController
 
-  def name_method
-    input_name = params["name"]
-    render json: {}
-
+  def name_game
+    input_name = params["name"].upcase
+    render json: {message: "#{input_name}!"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   
-  get "/params_game" => "params#name_method"
+  get "/params_game" => "params#name_game"
 
 end

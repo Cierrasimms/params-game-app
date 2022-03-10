80c2ead25e4ad83798f36b8bca8690641103d14d
CierraSimms <cierrasimms98@gmail.com>
2022-03-09 20:24:29 -0500

    added new if statement to name_game method

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,7 +1,11 @@
 class ParamsController < ApplicationController
-
-  def name_game
-    input_name = params["name"].upcase
-    render json: {message: "#{input_name}!"}
+    
+  def name_game  
+  input_name = params["name"].upcase
+    if input_name.starts_with?("A")
+      render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
+    else
+     render json: {message: "#{input_name} "}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   
   get "/params_game" => "params#name_game"
-
 end
